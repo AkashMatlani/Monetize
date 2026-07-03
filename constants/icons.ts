02ff4a9c8c39db3d9cb27859d63b13ae90545266
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-07-03 12:06:34 +0100

    feat: implement tab navigation with custom icons and styles

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,8 +1,15 @@
 
+import activity from "@/assets/icons/activity.png";
 import home from "@/assets/icons/home.png";
+import setting from "@/assets/icons/setting.png";
+import wallet from "@/assets/icons/wallet.png";
+
 
 export const icons = {
-    home
+    home,
+    wallet,
+    activity,
+    setting,
 } as const;
 
 export type IconKey = keyof typeof icons;
