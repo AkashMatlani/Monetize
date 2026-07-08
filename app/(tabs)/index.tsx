15b232ae51bd71ae83596da8e75104e941abb42c
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-07-08 11:32:46 +0100

    feat: add balance display with currency formatting and new add icon, update styles for balance card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,8 @@
+import { icons } from '@/constants/icons';
 import images from '@/constants/images';
 import '@/global.css';
+import { formatCurrency } from '@/lib/utills';
+import dayjs from "dayjs";
 import { styled } from "nativewind";
 import { Image, Text, View } from 'react-native';
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
@@ -8,12 +11,21 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-     <View className='home-header'>
-      <View className='home-user'>
-        <Image source={images.avatar} className='home-avatar'/>
-        <Text className='home-user-name'>Akash Matlani</Text>
+      <View className='home-header'>
+        <View className='home-user'>
+          <Image source={images.avatar} className='home-avatar' />
+          <Text className='home-user-name'>Akash Matlani</Text>
+        </View>
+        <Image source={icons.add} className='home-add-icon'/>
       </View>
-     </View>
+      <View className='home-balance-card'>
+        <Text className='home-balance-label'>Balance</Text>
+
+        <View className='home-balance-row'>
+          <Text className='home-balance-amount'>{formatCurrency(2489.48)}</Text>
+          <Text className='home-balance-date'>{dayjs("2026-03-18T09:00:00.000Z").format('MM/DD')}</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,15 +1,16 @@
 
 import activity from "@/assets/icons/activity.png";
+import add from "@/assets/icons/add.png";
 import home from "@/assets/icons/home.png";
 import setting from "@/assets/icons/setting.png";
 import wallet from "@/assets/icons/wallet.png";
-
 
 export const icons = {
     home,
     wallet,
     activity,
     setting,
+    add,
 } as const;
 
 export type IconKey = keyof typeof icons;
