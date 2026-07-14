8363887761971da709bf28efd6c62010fda27f73
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-07-14 11:36:44 +0100

    feat: enhance SubscrptionCard with additional details and integrate UpcomingSubscrptionCard in the main view

--- components/SubscrptionCard.tsx
+++ components/SubscrptionCard.tsx
@@ -1,9 +1,9 @@
-import { formatCurrency, formatDateSubscrptionDateTime } from '@/lib/utills'
+import { formatCurrency, formatDateSubscrptionDateTime, formatSatusLabel } from '@/lib/utills'
 import clsx from 'clsx'
 import React from 'react'
 import { Image, Pressable, Text, View } from 'react-native'
 
-const SubscrptionCard = ({ name, price, currency, icon, billing, color, category, plan, renewalDate, expanded, onPress, paymentMethod }: SubscrptionCardProps) => {
+const SubscrptionCard = ({ name, price, currency, icon, billing, color, category, plan, renewalDate, expanded, onPress, paymentMethod, startDate, status }: SubscrptionCardProps) => {
     return (
         <Pressable onPress={onPress} className={clsx('sub-card', expanded ? 'sub-card-expanded' : 'bg-card')}
             style={!expanded && color ? { backgroundColor: color } : undefined}>
@@ -19,7 +19,6 @@ const SubscrptionCard = ({ name, price, currency, icon, billing, color, category
                         </Text>
                     </View>
                 </View>
-
                 <View className='sub-price-box'>
                     <Text className='sub-price'>{formatCurrency(price, currency)}</Text>
                     <Text className='sub-billing'>{billing}</Text>
@@ -27,13 +26,40 @@ const SubscrptionCard = ({ name, price, currency, icon, billing, color, category
             </View>
             {expanded && (
                 <View className='sub-body'>
-                    <View className='sub-row'>
-                        <View className='sub-row-copy'>
-                            <Text className='sub-label'>Payemnt:</Text>
-                            <Text className='sub-value' numberOfLines={1} ellipsizeMode='tail'>{paymentMethod?.trim()}</Text>
+                    <View className='sub-details'>
+                        <View className='sub-row'>
+                            <View className='sub-row-copy'>
+                                <Text className='sub-label'>Payemnt:</Text>
+                                <Text className='sub-value' numberOfLines={1} ellipsizeMode='tail'>{paymentMethod?.trim()}</Text>
+                            </View>
+                        </View>
+                        <View className='sub-row'>
+                            <View className='sub-row-copy'>
+                                <Text className='sub-label'>Category:</Text>
+                                <Text className='sub-value' numberOfLines={1} ellipsizeMode='tail'>{category?.trim() || plan?.trim()}</Text>
+                            </View>
+                        </View>
+                        <View className='sub-row'>
+                            <View className='sub-row-copy'>
+                                <Text className='sub-label'>Started:</Text>
+                                <Text className='sub-value' numberOfLines={1} ellipsizeMode='tail'>{startDate ? formatDateSubscrptionDateTime(startDate) : ''}</Text>
+                            </View>
+                        </View>
+                        <View className='sub-row'>
+                            <View className='sub-row-copy'>
+                                <Text className='sub-label'>Renewal date:</Text>
+                                <Text className='sub-value' numberOfLines={1} ellipsizeMode='tail'>{renewalDate ? formatDateSubscrptionDateTime(renewalDate) : ''}</Text>
+                            </View>
+                        </View>
+                        <View className='sub-row'>
+                            <View className='sub-row-copy'>
+                                <Text className='sub-label'>Status:</Text>
+                                <Text className='sub-value' numberOfLines={1} ellipsizeMode='tail'>{status ? formatSatusLabel(status) : ''}</Text>
+                            </View>
                         </View>
                     </View>
                 </View>
+
             )}
         </Pressable>
     )
